dd0e9f74ad12b467806c7f4f63748aaa11cb3b7b
gautam591 <gautambohara591@gmail.com>
2023-10-24 22:21:07 +0900

    Notification page modified

--- lib/screens/notification.dart
+++ lib/screens/notification.dart
@@ -2,8 +2,33 @@ import 'package:flutter/material.dart';
 class notification extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Page'),
+    return Scaffold(
+      body: NotificationList(),
+    );
+  }
+}
+
+class NotificationList extends StatelessWidget {
+  final List<String> notifications = [
+    'Your item biscuit is expired today which was bought on 2023/1/22',
+    'Your item colla batch bought on 2023/1/23 is expiring soon on 2023/12/12',
+    'Your item biscuit is expired today which was bought on 2023/1/22',
+    'Your item colla batch bought on 2023/1/23 is expiring soon on 2023/12/12',
+    'Your item biscuit is expired today which was bought on 2023/1/22',
+    'Your item colla batch bought on 2023/1/23 is expiring soon on 2023/12/12',
+    'Your stoke is getting less please check on it',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: notifications.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(notifications[index]),
+          leading: Icon(Icons.notifications,color: Colors.redAccent,),
+        );
+      },
     );
   }
 }
